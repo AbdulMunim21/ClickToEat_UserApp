222904d1a674b64837004875030cfd6f34d1eebb
Abdul Munim <amunim1999@gmail.com>
2021-12-23 18:01:35 +0500

    Basic Structure of ClickToEat

---
+++ screens/FeedbackScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default FeedBackScreen = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({
+    root:{}
+})
